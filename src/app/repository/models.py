8a5b9fe50f1ce0c6cf69f8a03769924b096e186d
amadeo <risefenixpuryfire@gmail.com>
2025-02-13 20:50:07 +0300

    Created and managed base models for APP

--- src/app/repository/models.py
+++ src/app/repository/models.py
@@ -1,8 +1,71 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from .database import Base
+from sqlalchemy import BIGINT, VARCHAR, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.app.repository.database import Base, engine, SessionLocal
 
-class User(Base):
-    __tablename__ = "users"
+class Student(Base):
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    telegram_id: Mapped[int] = mapped_column(BIGINT, unique=True)
+    created_at = None
+    updated_at = None
+    user_credentials: Mapped['User_credentials'] = relationship(
+        back_populates="student", uselist=False, cascade="all, delete-orphan"
+    )
+    telegram_profile: Mapped["TelegramProfile"] = relationship(
+        back_populates="student", uselist=False, cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"Student= {self.telegram_id}, created_at= {self.created_at}, updated_at= {self.updated_at}"
+
+
+class User_credentials(Base):
+    """
+    Represents a user's credentials model with next attributes
+    for telegram registration.
+    id, created_at, updated_at were inherited by Base
+    id : int
+    username : str
+    password : str
+    created_at : datastamp
+    updated_at : datastamp
+    REFERS to students table
+    """
+    username: Mapped[str] = mapped_column(VARCHAR)
+    password: Mapped[str] = mapped_column(VARCHAR)
+    student_id: Mapped[int] = mapped_column(
+        BIGINT, ForeignKey('students.id'), unique=True, nullable=False
+    )
+
+    student: Mapped['Student'] = relationship(back_populates="user_credentials")
+
+    def __repr__(self) -> str:
+        return f"UserCredentials(id={self.id}, username={self.username}, password={self.password})"
+
+
+class TelegramProfile(Base):
+    student_id: Mapped[int] = mapped_column(
+        BIGINT, ForeignKey('students.id'), unique=True, nullable=False
+    )
+    name: Mapped[str] = mapped_column(VARCHAR)
+    language: Mapped[str] = mapped_column(VARCHAR)
+
+    student: Mapped["Student"] = relationship(back_populates="telegram_profile")
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+async def create_and_commit():
+    # Создаем сессию
+    async with SessionLocal() as session:
+        # Создаем объекты
+        s = Student(telegram_id=112312312, user_credentials=User_credentials(username="test", password="<PASSWORD>"),
+                    telegram_profile=TelegramProfile(name='123', language='ru'))
+        # Добавляем объекты в сессию
+        session.add(s)
+
+        # Выполняем commit
+        await session.commit()
+
+        # После commit объекты будут сохранены, и id будут присвоены
+        print(s.user_credentials)  # Теперь id будет не None
--- src/main.py
+++ src/main.py
@@ -1,33 +1,9 @@
-# import asyncio
-# from src.app.keyboards.keyboards import keyboard
-# from src.app.handlers.handlers import register_handlers
-# from src.bot_instance import bot
-# from src.app.repository.database import reset_database
-#
-# async def start_bot():
-#     await keyboard.set_menu()
-#     # await reset_database()
-#     register_handlers(bot)
-#     print("Bot is running...")
-#     await bot.polling()
-#
-# async def main():
-#     await reset_database()
-#     await start_bot()
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-import asyncio
-from src.app.repository.database import init_db, SessionLocal
-from src.app.repository.models import User
+from src.app.repository.models import init_db, create_and_commit
 
 async def main():
-    await init_db()  # Инициализация базы данных
+    await init_db()
+    await create_and_commit()
 
-    async with SessionLocal() as session:
-        new_user = User(name="Alice")
-        session.add(new_user)
-        await session.commit()
-
-asyncio.run(main())
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
